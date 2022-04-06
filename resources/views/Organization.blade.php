18758ff4ec48f4848573cabc3981aa8954ed4ced
SylarKilmister <sylarkilmister@gmail.com>
2022-04-05 19:53:32 -0500

    organigrama

--- resources/views/Organization.blade.php
+++ resources/views/Organization.blade.php
@@ -35,52 +35,51 @@
             </div>
         </div>
         </div>
-    <div class="portfolio-header"> <span class="color"> My </span> Portfolio
-    <span class ="header-caption"> Some Of My <span class="color"> Works</span></span></div>
+    <div class="portfolio-header"> <span class="color"> Diagrama  </span> Organizacional
+    <span class ="header-caption"> Oficina del  <span class="color"> Administrador </span></span></div>
      <div id="portfolio-content">
          <div class="portfolio portfolio-first">
              <div class="portfolio-image">
-                <img src="images/portfolio-first.jpg" alt="portfolio-first">
+                <img src="images/bill.jpg" alt="portfolio-first">
              </div>
              <div class="portfolio-text">
-                 <h2>App Idea</h2>
-                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad ut optio repellat cupiditate
-                     expedita eius dignissimos. Id cumque placeat minima ad laudantium suscipit voluptatem ducimus</p>
-                 <div class="button"><a href="#"><button><span class="index"> View Project<i class="gg-arrow-right"></i></span></button></a></div>
+                 <h2>Bill Nelson</h2>
+                 <h2 style="color: white">Administrator</h2>
+                 <div class="button"><a href="#"><button><span class="index"> View Carrer<i class="gg-arrow-right"></i></span></button></a></div>
              </div>
          </div>
          <div class="portfolio portfolio-second">
             <div class="portfolio-image">
-                <img src="images/portfolio-second.jpg" alt="portfolio-second">
+                <img src="images/pam.jpg" alt="portfolio-second">
             </div>
             <div class="portfolio-text">
-                <h2>Web Designing</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id cumque placeat minima ad laudantium suscipit
-                     voluptatem ducimus. Id cumque placeat minima ad laudantium suscipit voluptatem ducimus</p>
-                <div class="button"><a href="#"><button><span class="index"> View Project<i class="gg-arrow-right"></i></span></button></a></div>
+                <h2>Pam Melroy</h2>
+                <h2 style="color: white">Deputy Administrator</h2>
+                <div class="button"><a href="#"><button><span class="index"> View Carrer<i class="gg-arrow-right"></i></span></button></a></div>
+            </div>
             </div>
         </div>
 
         <div class="portfolio portfolio-third">
             <div class="portfolio-image">
-                <img src="images/portfolio-third.jpg" alt="portfolio-third">
+                <img src="images/robert.jpg" alt="portfolio-third">
             </div>
             <div class="portfolio-text">
-                <h2>UI Designing</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad ut optio repellat cupiditate expedita eius dignissimos
-                    .. Id cumque placeat minima ad laudantium suscipit voluptatem ducimus</p>
-                <div class="button"><a href="#"><button><span class="index"> View Project<i class="gg-arrow-right"></i></span></button></a></div>
+                <h2>Robert Cabana</h2>
+                <h2 style="color: white">Associate Administrator</h2>
+                <div class="button"><a href="#"><button><span class="index"> View Carrer<i class="gg-arrow-right"></i></span></button></a></div>
+            </div>
             </div>
         </div>
         <div class="portfolio portfolio-fourth">
             <div class=" portfolio-image">
-                <img src="images/portfolio-fourth.jpg" alt="portfolio-fourth">
+                <img src="images/susi.jpg" alt="portfolio-fourth">
             </div>
             <div class="portfolio-text">
-                <h2>Wow Graphics</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad ut optio repellat cupiditate
-                     expedita eius dignissimos. Id cumque placeat minima ad laudantium suscipit voluptatem ducimus</p>
-                <div class="button"><a href="#"><button><span class="index"> View Project<i class="gg-arrow-right"></i></span></button></a></div>
+                <h2>Susie Perez Quinn</h2>
+                <h2 style="color: white">Chief of Staff</h2>
+                <div class="button"><a href="#"><button><span class="index"> View Carrer<i class="gg-arrow-right"></i></span></button></a></div>
+            </div>
             </div>
         </div>
          </div>
