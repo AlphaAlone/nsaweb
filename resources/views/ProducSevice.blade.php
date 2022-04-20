0b35687eb0c65e8fff57760b81b3d344981fca7a
SylarKilmister <sylarkilmister@gmail.com>
2022-04-20 18:21:12 -0500

    actualizacion pestaba blogs

--- resources/views/ProducSevice.blade.php
+++ resources/views/ProducSevice.blade.php
@@ -34,8 +34,8 @@
             </div>
         </div>
         </div>
-<div class="blog-header"> Blogs</span>
-    <span class ="header-caption"> My Latest <span class="color"> blog posts.</span></span></div>
+<div class="blog-header"> Productos</span>
+    <span class ="header-caption"> Mira nuestros <span class="color"> diferentes paquetes.</span></span></div>
     <div class="blog-content">
          <div class="blogs">
              <a href="#">
@@ -44,7 +44,7 @@
                 <div class="blog-date">8 May,20</div>
              </div>
              <div class="blog-text">
-                 <h3>Harleys In Hawaai</h3>
+                 <h3>Lorem ipsum</h3>
                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus alias dolore recusandae illum, corrupti quo
                      veniam saepe aliquid! Quis voluptates ratione consequuntur vel, perferendis cum provident? Magnam fugiat voluptas
                       libero.</p>
@@ -57,7 +57,7 @@
                 <div class="blog-date">16 Jan,20</div>
             </div>
             <div class="blog-text">
-                <h3>Key To Be Productive</h3>
+                <h3>Lorem ipsum</h3>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt maiores,
                     recusandae cupiditate ducimus a non tempora, architecto obcaecati eaque ipsum assumenda harum dolorum iusto tenetur
                      eius eligendi dolor magnam sit!</p>
@@ -70,7 +70,7 @@
                 <div class="blog-date">30 Nov,19</div>
             </div>
             <div class="blog-text">
-                <h3>Caffeine Addict</h3>
+                <h3>Lorem ipsum</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo nostrum impedit
                     ipsam perspiciatis ratione sapiente quasi optio reprehenderit, labore consequuntur suscipit cum quas.
                      Officiis dolorem asperiores, ut necessitatibus quas doloremque?</p>
@@ -83,7 +83,7 @@
                 <div class="blog-date">6 Jul,19</div>
             </div>
             <div class="blog-text">
-                <h3>Web Development</h3>
+                <h3>Lorem ipsum</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, veniam ratione quam vitae,
                      quibusdam explicabo rem debitis velit ipsa repellat, impedit nulla fuga? Amet corporis praesentium quae.
                       Sed, quibusdam necessitatibus.</p>
@@ -96,7 +96,7 @@
                 <div class="blog-date">1 Jun,19</div>
             </div>
             <div class="blog-text">
-                <h3>Work From Home</h3>
+                <h3>Lorem ipsum</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt eum necessitatibus rem
                     dignissimos nulla mollitia cumque, provident officiis non vitae? Animi aut doloremque illum, soluta hic minus
                     sint explicabo..</p>
@@ -109,7 +109,7 @@
                 <div class="blog-date">28 Feb,19</div>
             </div>
             <div class="blog-text">
-                <h3>Business Trip</h3>
+                <h3>Lorem ipsum</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempora dolorum fuga ratione, unde,
                     ex quaerat iste numquam nemo nihil nobis rem sint quia recusandae dignissimos quos ut rerum nam.</p>
             </div></a>
