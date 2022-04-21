ddeb0f87172be3f059dcd8bcee2591a516d99695
AlphaAlone <fitoalfavzqmail.com>
2022-04-20 20:02:11 -0500

    Un error lo comete cualquiera xdd

--- resources/views/partials/nav_nsaweb.blade.php
+++ resources/views/partials/nav_nsaweb.blade.php
@@ -11,7 +11,7 @@
         <a href="{{ route('home') }}" data-text="INICIO" id="home-link" >INICIO</a>
         <a href="{{ route('about') }}" data-text="SOBRE NOSOTROS" id="about-link" >SOBRE NOSOTROS</a>
         <a href="{{ route('product') }}" data-text="PRODUCTOS Y SERVICIOS" id="blog-link" >PRODUCTOS Y SERVICIOS</a>
-        <a href="{{ route('organi') }}" data-text="ORGANISACION" id="portfolio-link" >ORGANISACION</a>
+        <a href="{{ route('organi') }}" data-text="ORGANIZACION" id="portfolio-link" >ORGANISACION</a>
         <a href="{{ route('contact') }}" data-text="CONTACTO" id="contact-link" >CONTACTO</a>
     </div>
 </div>
